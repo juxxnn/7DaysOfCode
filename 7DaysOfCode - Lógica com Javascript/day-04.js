6e1e3fdb5cc804e6d17372e143c86fd920fa28c1
Juliana Silva <julianastankovic@gmail.com>
2022-03-26 23:28:29 -0300

    Lógica JS 4/7 ��‍ M Loops e Randomização

--- 7DaysOfCode - Lógica com Javascript/day-04.js
+++ 7DaysOfCode - Lógica com Javascript/day-04.js
@@ -4,18 +4,18 @@ var chances = 1;
 
 alert("* * * JOGO DE ADIVINHA * * *\nAdivinhe o número que o computador está pensando...")
 
-while(chances < 4){
+while (chances < 4) {
 
     const valor = prompt("Digite um número de 0 a 10: ")
 
-if (valor == numero) {
-    alert("Parabéns! Você adivinhou. O número era "+numero+".")
-    break;
-}
-else{
-    alert("Você errou. Tente novamente. \nVocê possui 3 tentativas e já realizou "+chances+ ".")
-    chances = chances + 1;
-}
+    if (valor == numero) {
+        alert("Parabéns! Você adivinhou. O número era " + numero + ".")
+        break;
+    }
+    else {
+        alert("Você errou. Tente novamente. \nVocê possui 3 tentativas e já realizou " + chances + ".")
+        chances = chances + 1;
+    }
 
 }
 alert("Fim do jogo!")
